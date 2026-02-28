72388d443c03c41341de399440576cd8d69a6d6e
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-02-27 21:23:27 -0600

    setup server with necessary middleware and passport.js session storage

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import session from "express-session";
+import passport from "passport";
+import passportConfig from "./config/passport.js";
+import router from "./routes/router.js";
+import connectPgSimple from "connect-pg-simple";
+import pool from "./db/pool.js";
 
-import indexRouter from "./routes/indexRouter.js";
+const pgSession = connectPgSimple(session);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,14 +17,34 @@ const assetsPath = path.join(__dirname, "public");
 
 const app = express();
 const PORT = 3000;
-
 app.use(express.static(assetsPath));
-app.use(express.urlencoded({ extended: true }));
-
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use("/", indexRouter);
+app.use(
+	session({
+		store: new pgSession({
+			pool: pool,
+			tableName: "user_sessions",
+		}),
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day
+	}),
+);
+
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+passportConfig(passport);
+
+//Makes the current logged in user variable available on the views
+app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
+	next();
+});
+
+app.use("/", router);
 
 app.use((req, res) => {
 	res.status(404).send("404 not found");
@@ -29,7 +55,7 @@ app.use((err, req, res, next) => {
 	res.status(500).send("500 server error");
 });
 
-app.listen(PORT, (error) => {
+app.listen(PORT || 3000, (error) => {
 	if (error) {
 		throw error;
 	}
