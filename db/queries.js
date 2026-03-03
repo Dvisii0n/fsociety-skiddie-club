38bb190862864debad63eab264c95bcdd01b38a5
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-03-02 20:21:17 -0600

    added order by desc to get all messages query, added create and delete message

--- db/queries.js
+++ db/queries.js
@@ -17,7 +17,7 @@ async function findUserByUsername(username) {
 
 async function getAllMessages() {
 	const { rows } = await pool.query(
-		"SELECT username AS author, title, body, date_created FROM messages JOIN users ON messages.user_id = users.id",
+		"SELECT messages.id, username AS author, title, body, date_created FROM messages JOIN users ON messages.user_id = users.id ORDER BY date_created DESC",
 	);
 
 	return rows;
@@ -29,9 +29,21 @@ async function updateMemberStatus(userId) {
 	]);
 }
 
+async function createMessage(userId, title, body) {
+	await pool.query(
+		"INSERT INTO messages (title, body, user_id) VALUES ($1, $2, $3)",
+		[title, body, userId],
+	);
+}
+
+async function deleteMessage(messageId) {
+	await pool.query("DELETE FROM messages WHERE id = $1", [messageId]);
+}
 export default {
 	createUser,
 	findUserByUsername,
 	getAllMessages,
 	updateMemberStatus,
+	createMessage,
+	deleteMessage,
 };
